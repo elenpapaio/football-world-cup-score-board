bf0c4bd9a0f14dbdb8f82a9e105bb5603b4e3cc7
Eleni Papaioannou <eleni.papaioannou90@gmail.com>
2024-06-12 23:23:35 +0300

    added startGame implementation - passing unit test

--- src/main/java/com/sportradar/service/GameService.java
+++ src/main/java/com/sportradar/service/GameService.java
@@ -1,11 +1,24 @@
 package com.sportradar.service;
 
 import com.sportradar.model.Game;
+import com.sportradar.model.Team;
+import com.sportradar.util.InputUtils;
 
 public class GameService {
 
     public Game startGame() {
-        return null;
+        String homeTeamName = InputUtils.readStringFromKeyboard();
+        String awayTeamName = InputUtils.readStringFromKeyboard();
+        return Game.builder()
+                .homeTeam(Team.builder()
+                        .name(homeTeamName)
+                        .build())
+                .awayTeam(Team.builder()
+                        .name(awayTeamName)
+                        .build())
+                .awayTeamScore(0)
+                .homeTeamScore(0)
+                .build();
     }
 
 }
