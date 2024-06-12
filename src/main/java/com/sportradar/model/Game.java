b6e70f991d35f7466500402182194d0c63162b99
Eleni Papaioannou <eleni.papaioannou90@gmail.com>
2024-06-12 22:47:47 +0300

    added Game & Team models

---
+++ src/main/java/com/sportradar/model/Game.java
@@ -0,0 +1,16 @@
+package com.sportradar.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Game {
+
+    private int gameId;
+    private Team homeTeam;
+    private Team awayTeam;
+    private int homeTeamScore;
+    private int awayTeamScore;
+
+}
---
+++ src/main/java/com/sportradar/model/Team.java
@@ -0,0 +1,12 @@
+package com.sportradar.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Team {
+
+    private String name;
+
+}
