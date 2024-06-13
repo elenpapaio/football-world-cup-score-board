a5efbba67112177d15a18a3126dcc3109172a6e2
Eleni Papaioannou <eleni.papaioannou90@gmail.com>
2024-06-13 21:44:48 +0300

    added insertGame implementation

--- src/main/java/com/sportradar/data/InMemoryDatabase.java
+++ src/main/java/com/sportradar/data/InMemoryDatabase.java
@@ -1,11 +1,39 @@
 package com.sportradar.data;
 
 import com.sportradar.model.Game;
+import com.sportradar.model.Team;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InMemoryDatabase {
+    @Getter
+    private final List<Game> games = new ArrayList<>();
+    private int newId = 1;
 
     public Game insertGame(Game game) {
-        return game;
+        Game objectToInsert = Game.builder()
+                .gameId(getNewId())
+                .homeTeam(Team.builder()
+                        .name(game.getHomeTeam()
+                                .getName())
+                        .build())
+                .awayTeam(Team.builder()
+                        .name(game.getAwayTeam()
+                                .getName())
+                        .build())
+                .awayTeamScore(game.getAwayTeamScore())
+                .homeTeamScore(game.getHomeTeamScore())
+                .build();
+        games.add(objectToInsert);
+        return objectToInsert;
+    }
+
+    private int getNewId() {
+        int id = newId;
+        newId = newId + 1;
+        return id;
     }
 
 }
