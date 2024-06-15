1616de04d58072ab9f2e8fe2ac3c5c73512b0da4
Eleni Papaioannou <eleni.papaioannou90@gmail.com>
2024-06-16 02:42:52 +0300

    added top level try-catch

--- src/main/java/com/sportsdatacompany/FootballWorldCupScoreBoardApplication.java
+++ src/main/java/com/sportsdatacompany/FootballWorldCupScoreBoardApplication.java
@@ -5,41 +5,53 @@ import com.sportsdatacompany.repository.GameRepository;
 import com.sportsdatacompany.service.GameService;
 import com.sportsdatacompany.util.InputUtils;
 
+import java.util.Arrays;
+
 public class FootballWorldCupScoreBoardApplication {
 
-	public static void main(String[] args) {
-		GameService gameService = new GameService(new GameRepository(new InMemoryDatabase()));
+    public static void main(String[] args) {
+        GameService gameService = new GameService(new GameRepository(new InMemoryDatabase()));
 
-		boolean exit = false;
+        boolean exit = false;
 
-		do {
-			System.out.println("Please choose an operation to perform: ");
-			System.out.println("1 -> start a game");
-			System.out.println("2 -> finish a game");
-			System.out.println("3 -> update the score of a game");
-			System.out.println("4 -> game summary sorted by total score");
-			System.out.println("5 -> exit");
-			int option = InputUtils.readIntFromKeyboard("");
-			switch (option) {
-				case 1:
-					gameService.startGame();
-					break;
-				case 2:
-					gameService.finishGame();
-					break;
-				case 3:
-					gameService.updateScore();
-					break;
-				case 4:
-					gameService.getGamesByTotalScore();
-					break;
-				case 5:
-					exit = true;
-					break;
-				default:
-					throw new RuntimeException("Invalid operation number.");
-			}
-		} while (!exit);
-	}
+        do {
+            System.out.println("Please choose an operation to perform: ");
+            System.out.println("1 -> start a game");
+            System.out.println("2 -> finish a game");
+            System.out.println("3 -> update the score of a game");
+            System.out.println("4 -> game summary sorted by total score");
+            System.out.println("5 -> exit");
+
+            try {
+                int option = InputUtils.readIntFromKeyboard("");
+                switch (option) {
+                    case 1:
+                        gameService.startGame();
+                        break;
+                    case 2:
+                        gameService.finishGame();
+                        break;
+                    case 3:
+                        gameService.updateScore();
+                        break;
+                    case 4:
+                        gameService.getGamesByTotalScore();
+                        break;
+                    case 5:
+                        exit = true;
+                        break;
+                    default:
+                        throw new RuntimeException("Invalid operation number.");
+                }
+            } catch (Exception e) {
+                if (e.getMessage() != null) {
+                    System.out.println(e.getMessage());
+                } else {
+                    System.out.println(e.getClass());
+                    System.out.println(Arrays.toString(e.getStackTrace()));
+                }
+            }
+        } while (!exit);
+    }
 
 }
