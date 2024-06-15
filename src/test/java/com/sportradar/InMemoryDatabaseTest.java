bf2ab138f8b9e575d61d9332665ab92b586e8a3c
Eleni Papaioannou <eleni.papaioannou90@gmail.com>
2024-06-15 18:36:14 +0300

    added failing unit tests for findGameById and updateGame

--- src/test/java/com/sportradar/InMemoryDatabaseTest.java
+++ src/test/java/com/sportradar/InMemoryDatabaseTest.java
@@ -1,6 +1,7 @@
 package com.sportradar;
 
 import com.sportradar.data.InMemoryDatabase;
+import com.sportradar.dto.GameDto;
 import com.sportradar.model.Game;
 import com.sportradar.model.Team;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -47,7 +49,7 @@ public class InMemoryDatabaseTest {
     }
 
     @Test
-    @DisplayName("deleteGameById - The game with the given id exists, so it should be deleted")
+    @DisplayName("deleteGameById - It should delete the game with the given id when it exists in the database")
     public void deleteGameById_test1() {
         Game game1 = Game.builder()
                 .gameId(1)
@@ -73,8 +75,7 @@ public class InMemoryDatabaseTest {
                 .homeTeamScore(0)
                 .build();
 
-        inMemoryDatabase.getGames()
-                .addAll(new ArrayList<>(Arrays.asList(game1, game2)));
+        inMemoryDatabase.getGames().addAll(new ArrayList<>(Arrays.asList(game1, game2)));
 
         Game deletedGame = inMemoryDatabase.deleteGameById(2);
 
@@ -83,7 +84,7 @@ public class InMemoryDatabaseTest {
     }
 
     @Test
-    @DisplayName("deleteGameById - The game with the given id does not exist, so it should be return null")
+    @DisplayName("deleteGameById - It should return null when the game with the given id does not exist in the database")
     public void deleteGameById_test2() {
         Game game = Game.builder()
                 .gameId(1)
@@ -97,13 +98,114 @@ public class InMemoryDatabaseTest {
                 .homeTeamScore(0)
                 .build();
 
-        inMemoryDatabase.getGames()
-                .add(game);
+        inMemoryDatabase.getGames().add(game);
 
         Game deletedGame = inMemoryDatabase.deleteGameById(4);
 
         assertNull(deletedGame);
         assertEquals(inMemoryDatabase.getGames().size(), 1);
+    }
+
+    @Test
+    @DisplayName("findGameById - It should find the game with the given id when it exists in the database")
+    public void findGameById_test1() {
+        Game game = Game.builder()
+                .gameId(1)
+                .homeTeam(Team.builder()
+                        .name("Italy")
+                        .build())
+                .awayTeam(Team.builder()
+                        .name("Uruguay")
+                        .build())
+                .homeTeamScore(1)
+                .awayTeamScore(0)
+                .build();
+
+        inMemoryDatabase.getGames().add(game);
+
+        Optional<Game> retrievedGameOptional = inMemoryDatabase.findGameById(1);
+        Game retrievedGame = retrievedGameOptional.orElse(null);
+
+        assertEquals(retrievedGame, game);
+    }
+
+    @Test
+    @DisplayName("findGameById - It should return an empty optional when the given game id does not exist in the database")
+    public void findGameById_test2() {
+        Game game = Game.builder()
+                .gameId(1)
+                .homeTeam(Team.builder()
+                        .name("Italy")
+                        .build())
+                .awayTeam(Team.builder()
+                        .name("Uruguay")
+                        .build())
+                .homeTeamScore(1)
+                .awayTeamScore(0)
+                .build();
+
+        inMemoryDatabase.getGames().add(game);
+
+        Optional<Game> retrievedGameOptional = inMemoryDatabase.findGameById(5);
+
+        assertEquals(retrievedGameOptional, Optional.empty());
+    }
+
+    @Test
+    @DisplayName("updateGame - It should update the game with the given id when it exists in the database")
+    public void updateGame_test1() {
+        Game game = Game.builder()
+                .gameId(2)
+                .homeTeam(Team.builder()
+                        .name("Spain")
+                        .build())
+                .awayTeam(Team.builder()
+                        .name("Brazil")
+                        .build())
+                .homeTeamScore(0)
+                .awayTeamScore(0)
+                .build();
+
+        inMemoryDatabase.getGames().add(game);
+
+        Game updatedGame = inMemoryDatabase.updateGame(GameDto.builder()
+                .gameId(2)
+                .homeTeamScore(1)
+                .awayTeamScore(1)
+                .build());
+
+        assertThat(updatedGame)
+                .usingRecursiveComparison()
+                .ignoringFields("homeTeamScore", "awayTeamScore")
+                .isEqualTo(game);
+        assertEquals(updatedGame.getHomeTeamScore(), 1);
+        assertEquals(updatedGame.getAwayTeamScore(), 1);
+    }
+
+    @Test
+    @DisplayName("updateGame - It should return null when the game with the given id does not exist in the database")
+    public void updateGame_test2() {
+        Game game = Game.builder()
+                .gameId(1)
+                .homeTeam(Team.builder()
+                        .name("Italy")
+                        .build())
+                .awayTeam(Team.builder()
+                        .name("Uruguay")
+                        .build())
+                .homeTeamScore(1)
+                .awayTeamScore(0)
+                .build();
+
+        inMemoryDatabase.getGames().add(game);
+
+        Game updatedGame = inMemoryDatabase.updateGame(GameDto.builder()
+                .gameId(2)
+                .homeTeamScore(1)
+                .awayTeamScore(1)
+                .build());
+
+        assertNull(updatedGame);
     }
 
 }
