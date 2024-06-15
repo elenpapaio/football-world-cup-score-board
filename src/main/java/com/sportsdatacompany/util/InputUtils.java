2b1b62ff61e82700026bdb8c066316c24c767601
Eleni Papaioannou <eleni.papaioannou90@gmail.com>
2024-06-16 02:27:07 +0300

    updated readIntFromKeyboard implementation for invalid user input

--- src/main/java/com/sportsdatacompany/util/InputUtils.java
+++ src/main/java/com/sportsdatacompany/util/InputUtils.java
@@ -1,5 +1,6 @@
 package com.sportsdatacompany.util;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputUtils {
@@ -13,7 +14,11 @@ public class InputUtils {
     public static int readIntFromKeyboard(String messageToUser) {
         System.out.println(messageToUser);
         Scanner keyboard = new Scanner(System.in);
-        return keyboard.nextInt();
+        try {
+            return keyboard.nextInt();
+        } catch (InputMismatchException e) {
+            throw new InputMismatchException("Invalid input - must be a number");
+        }
     }
 
 }
